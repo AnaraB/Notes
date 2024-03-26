ce1c51f1e8472fb5b93422c974df2db193c4aee4
Anara <berdali.aygerim9@gmail.com>
2024-03-26 20:41:33 +0000

    initial commit; dummy toDolist added, CssBaseline used

---
+++ reactnotes/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CssBaseline from "@mui/material/CssBaseline"
+
+function App() {
+
+  //CssBaseline removes all margines and paddings, resets css
+
+  return (
+   <>
+   <CssBaseline />
+   <h1>ToDos</h1>
+   </>
+  )
+}
+
+export default App
---
+++ reactnotes/src/TodoList.jsx
@@ -0,0 +1,11 @@
+const initialTodos = [
+  {id:1, text: "walk the dog", completed: false}, 
+  {id:2, text: "Cook lunch", completed: false}, 
+  {id:3, text: "walk the dog", completed: true}, 
+  {id:4, text: "shopping", completed: false}, 
+  {id:5, text: "walk the dog", completed: true}, 
+]
+
+export default function TodoList() {
+  return 
+}
